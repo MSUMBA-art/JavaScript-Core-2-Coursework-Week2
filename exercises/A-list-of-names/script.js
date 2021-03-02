cad5a1c7d77f224359a86646b314816d4b952a7c
Alexander Msumba <msumba83@gmail.com>
2021-03-03 00:08:25 +0200

    done exercise A and B

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,23 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  console.log(content); //
+}
+function listOfNames(arrayOfPeople) {
+  const content = document.querySelector("#content");
+  console.log("content", content);
+
+  arrayOfPeople.forEach((element) => {
+    console.log(element);
+
+    let nameHeading = document.createElement("h1");
+    nameHeading.innerText = element.name;
+
+    let jobHeading = document.createElement("h2");
+    jobHeading.innerText = element.job;
+
+    content.appendChild(nameHeading);
+    content.appendChild(jobHeading);
+  });
 }
 
 let people = [
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,20 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+
+  const shoppingItems = document.getElementById("content");
+  console.log(shoppingItems);
+
+  let ul = document.createElement("ul");
+
+  shopping.forEach((element) => {
+    console.log(element);
+
+    let li = document.createElement("li");
+    li.innerText = element;
+    ul.appendChild(li);
+
+    shoppingItems.appendChild(ul);
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
