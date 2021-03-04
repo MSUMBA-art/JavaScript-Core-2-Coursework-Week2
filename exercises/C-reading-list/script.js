c83152fb49a09f24a18f3cb776fc4109fe145869
Alexander Msumba <msumba83@gmail.com>
2021-03-04 07:13:48 +0200

    done reading-list

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,6 +1,34 @@
 function readingList(books) {
   // Write your code here...
+
+  var content = document.querySelector('#content')
+  for (let i=0; i<books.length; i++){
+    var ul = document.createElement('ul');
+    ul.style.display = "flex"
+    ul.style.alignItems = "center"
+    ul.style.justifyContent = "center"
+    ul.style.padding = "10px"
+    let li = document.createElement('li');
+    li.style.listStyle = "none"
+    if (books[i].alreadyRead) {
+      li.style.backgroundColor = "green";
+    } else {
+      li.style.backgroundColor = "red";
+    }
+    var p = document.createElement('p');
+    var img = document.createElement('img');
+    p.innerHTML = `${books[i].title} by ${books[i].author}`
+    p.style.textAlign = "center"
+    p.style.fontWeight = "bolder";
+    content.style.display = "flex"
+    img.src = books[i].img
+    img.style.marginTop = "20px"
+    img.style.height = "40%"
+    img.style.marginLeft = "10px"
+    content.appendChild(ul).appendChild(li).appendChild(p).appendChild(img);
+  }
 }
+
 
 const books = [
   {
