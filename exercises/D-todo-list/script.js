8a75cb4b58426846f1f9169a1e388b34de768dda
Alexander Msumba <msumba83@gmail.com>
2021-03-04 22:33:16 +0200

    done to-do-list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,26 @@
 function todoList(todos) {
   // Write your code here...
+  var content = document.querySelector('#content');
+  var ul = document.createElement('ul');
+  content.appendChild(ul);
+
+  todos.forEach((element) => {
+    let li = document.createElement('li');
+    li.innerText = element.todo;
+    ul.appendChild(li);
+
+    //adding event listener and line-through style
+    
+    li.addEventListener("click", strikeThrough);
+    function strikeThrough() {
+      if (li.style.textDecorationLine === 'line-through'){
+      li.style.textDecorationLine = 'initial';
+    } else {
+      li.style.textDecorationLine = 'line-through';
+    }
+    }
+    
+  })
 }
 
 const todos = [
